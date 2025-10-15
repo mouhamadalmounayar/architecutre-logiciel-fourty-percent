3f56e9eb634466da1ddbaaf3c9f9714c13079e04
Logan Brunet <98391380+Loboss2206@users.noreply.github.com>
2025-10-15 16:01:52 +0200

    fix: fix container name and add alert topic for blood pressure

--- local/src/sensors/iot-gateway.js
+++ local/src/sensors/iot-gateway.js
@@ -2,7 +2,8 @@ import mqtt from 'mqtt';
 
 const BROKER = process.env.MQTT_BROKER || 'localhost';
 const PORT = process.env.MQTT_PORT || 1883;
-const TOPIC = 'sensors/bloodPressure';
+const TOPIC_ALERT_BLOOD_PRESSURE = 'sensors/alert/bloodPressure';
+const TOPIC_REGULAR_BLOOD_PRESSURE = 'sensors/regular/bloodPressure';
 
 const client = mqtt.connect(`mqtt://${BROKER}:${PORT}`);
 
@@ -13,7 +14,15 @@ client.on('connect', () => {
         const bpm = Math.floor(60 + Math.random() * 40);
         const message = JSON.stringify({ bpm, timestamp: Date.now() });
 
-        client.publish(TOPIC, message);
+        client.publish(TOPIC_REGULAR_BLOOD_PRESSURE, message);
         console.log('BPM sent:', message);
     }, 2000);
+
+    setInterval(() => {
+        const bpm = Math.floor(60 + Math.random() * 40);
+        const message = JSON.stringify({ bpm, timestamp: Date.now() });
+
+        client.publish(TOPIC_ALERT_BLOOD_PRESSURE, message);
+        console.log('BPM ALERT:', message);
+    }, 20000);
 });
